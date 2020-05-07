b385067db3821a0a7a40f6b2aacd10f8b8579c5b
harolddavies <panjama231@gmail.com>
2020-05-07 12:01:49 -0500

    Initial Project Setup

---
+++ src/main/java/deadeyedick/ParkingComplexManager/ParkingComplexManagerApplication.java
@@ -0,0 +1,14 @@
+package deadeyedick.ParkingComplexManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingComplexManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingComplexManagerApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/deadeyedick/ParkingComplexManager/ParkingComplexManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package deadeyedick.ParkingComplexManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingComplexManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
